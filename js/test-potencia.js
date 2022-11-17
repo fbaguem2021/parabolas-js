6de42ae170d8e2235e2dac55b4fe8f385aa7fe58
Francesc Bagué Martí (cep) <fbaguem2021@cepnet.net>
2022-11-17 13:55:50 +0100

    creado test-potencia.html/js para intentar optimizar la barra de potencia

---
+++ js/test-potencia.js
@@ -0,0 +1,127 @@
+// CONSTANTES
+const btn = document.getElementById("btn");
+const fillbar = document.getElementById("fill");
+const fill = $('.progress-bar-fill');
+const line = $('.line');
+// BOOLEANO PARA PARAR LA ANIMACION
+let stopAnimation = false;
+// EVENTO DE CLIC PARA EL BOTÓN
+btn.onclick = function() {
+    fill.stop();
+    stopAnimation = true;
+    console.log(fillbar.style.width);
+}
+// EVENTO PARA DETECTAR LA PULSACIÓN DEL ESPACIO
+window.addEventListener("keydown", (key) => {
+    if (key.code == "Space") {
+        fill.stop();
+        line.stop();
+        stopAnimation = true;
+        console.log(fillbar.style.width);
+        console.log(document.getElementById("line").style.left);
+    }
+})
+
+// ejecuta la función de la animación
+//startAnimation();
+
+
+function loop() {
+    $('#clouds').css({right:0});
+    $('#clouds').animate({
+        right: '+=1400',
+    }, {
+        duration: 5000, 
+        easing: 'linear', 
+        complete: loop
+    });
+}
+loop();
+var position = 0;
+function myloop() {
+    //if(action=="increase"){position++;}else if(action=="decrease"){position--;}
+    //"increase"==action?position++:"decrease"==action&&position--;
+    //action=="increase"?position++:action=="decrease"&&position--;
+    // stop?console.log("Stopped"):
+    //     "increase"==action?position++:"decrease"==action&&position--;
+    if (position < 100) {
+        stop?console.log("Stopped"):
+            ("increase"==action?position+=50:"decrease"==action&&
+            position--,moveLine(myloop));
+    }
+}
+function moveLine(callback) {
+    $('.line').animate({
+        'left': `${position}%`,
+    }, {
+        duration: 2500,
+        easing: 'linear',
+        complete: callback
+    })
+}
+
+/**
+ * Funcion asincrona que inicia la animación de la barra
+ */
+async function startAnimation() {
+    console.log("Starting animation");
+    await animateBar("increase", 0);
+    await animateLine("increase", 0);
+}
+function animateLine(action, position) {
+    if (action == "increase") {
+        position++;
+    } else if (action == "decrease") {
+        position--;
+    }
+    setTimeout(() => {
+        line.animate({
+    	    'left': `${position}%`,
+        }, {
+    	    duration: 1,
+            complete: function() {
+                if (position == 100) {
+                    action = "decrease";
+                } else if (position == 0) {
+                    action = "increase";
+                }
+                if (stopAnimation == false) {
+                    //animateLine(action, position);
+                } else {
+                    console.log("Ending animation")
+                }
+            }
+        });
+    }, 50);
+}
+/**
+ * Función con la animación de la barra
+ * @param action Variable con la acción que se hará. Puede ser: "increase" para aumentar el valor o "decrease" para disminuir el valor
+ * @param width Valor de ancho que se le dara al relleno de la progress bar
+ */
+// function animateBar(action, width) {
+//     if (action == "increase") {
+//         width++;
+//     } else if (action == "decrease") {
+//         width--;
+//     }
+//     setTimeout(() => {
+//         fill.animate({
+//     	    'width': `${width}%`,
+//         }, {
+//     	    duration: 1,
+//             complete: function() {
+//                 if (width == 100) {
+//                     action = "decrease";
+//                 } else if (width == 0) {
+//                     action = "increase";
+//                 }
+//                 if (stopAnimation == false) {
+//                     animateBar(action, width);
+//                 } else {
+//                     console.log("Ending animation")
+//                 }
+//             }
+//         });
+//     }, 50);
+// }
